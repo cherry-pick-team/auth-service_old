003fd6ecc0a78c96451b39a23c71aa55ab93b330
Nikita <nks.off@gmail.com>
2017-03-31 10:43:07 +0300

    add facebook sdk for getting music

--- routes/default.php
+++ routes/default.php
@@ -6,14 +6,45 @@ use Laravel\Socialite\Facades\Socialite;
 
 // log in (via facebook)
 Route::get('/auth/facebook/redirect', function (Request $request) {
-    return Socialite::driver('facebook')->redirect();
+    return Socialite::driver('facebook')->scopes([
+        'email', 'user_likes',
+    ])->redirect();
 });
 
 // check user (via facebook)
 Route::get('/auth/facebook/check', function (Request $request) {
     $user = Socialite::driver('facebook')->user();
-    return '<pre>' . print_r($user, 1) . '</pre>';
+
+    return redirect()->route('facebook-token', ['token' => $user->token,]);
 });
+
+Route::get('/auth/facebook/token', function (Request $request) {
+    $token = $request->input('token');
+
+    $fb = new \Facebook\Facebook([
+        'app_id' => config('services.facebook.client_id'),
+        'app_secret' => config('services.facebook.client_secret'),
+        'default_graph_version' => 'v2.8',
+        'default_access_token' => $token,
+    ]);
+
+    $meRequest = $fb->get('/me');
+    $meObj = $meRequest->getGraphObject();
+    $id = $meObj->getField('id');
+    $name = $meObj->getField('name');
+
+    $request = $fb->get('/' . $id . '/music');
+    $edge = $request->getGraphEdge();
+
+    $res = '<pre>';
+    $res .= $name . PHP_EOL . PHP_EOL;
+    foreach ($edge as $node) {
+        $res .= print_r($node->getField('name'), 1) . PHP_EOL;
+    }
+    $res .= '</pre>';
+
+    return $res;
+})->name('facebook-token');
 
 // log in (via vk)
 Route::get('/auth/vk/redirect', function (Request $request) {
